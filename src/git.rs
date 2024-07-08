019b089e99369649af4fdfdcf2480eb7d0f42801
Simon Buchan <simon.buchan@skilitics.com>
2024-07-08 18:18:55 +1200

    find git root dir, parse and preserve files in index.

---
+++ src/git.rs
@@ -0,0 +1,222 @@
+use std::path::{Path, PathBuf};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Could not find git workspace from {}", .path.display())]
+    NoWorkspace { path: PathBuf },
+
+    #[error("could not parse index file")]
+    IndexParseError(#[from] IndexParseError),
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+pub struct Workspace {
+    pub root_dir: PathBuf,
+    pub git_dir: PathBuf,
+}
+
+impl Workspace {
+    pub fn find(dir: &Path) -> Result<Self> {
+        for dir in dir.ancestors() {
+            let git_dir = dir.join(".git");
+            if let Ok(git_metadata) = git_dir.metadata() {
+                if git_metadata.is_dir() {
+                    return Ok(Self {
+                        root_dir: dir.to_path_buf(),
+                        git_dir,
+                    });
+                } else {
+                    // .git is a file, so it should contain the path to the real git dir
+                    if let Ok(mut git_dir_relative_path) = std::fs::read_to_string(&git_dir) {
+                        git_dir_relative_path.pop(); // remove trailing newline
+                        return Ok(Self {
+                            root_dir: dir.to_path_buf(),
+                            git_dir: dir.join(git_dir_relative_path),
+                        });
+                    }
+                }
+            }
+        }
+        Err(Error::NoWorkspace {
+            path: dir.to_path_buf(),
+        })
+    }
+
+    pub fn index(&self) -> Result<Index> {
+        Ok(Index::parse(self.git_dir.join("index"))?)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum IndexParseError {
+    #[error("io error")]
+    Io(#[from] std::io::Error),
+    #[error("too short: {0}")]
+    TooShort(usize),
+    #[error("invalid signature")]
+    InvalidSignature,
+    #[error("unknown version: {0}")]
+    UnknownVersion(u32),
+}
+
+struct Reader<'a> {
+    data: &'a [u8],
+    offset: usize,
+}
+
+impl<'a> Reader<'a> {
+    fn new(data: &'a [u8]) -> Self {
+        Self { data, offset: 0 }
+    }
+
+    fn slice(&mut self, len: usize) -> &'a [u8] {
+        let slice = &self.data[self.offset..self.offset + len];
+        self.offset += len;
+        slice
+    }
+
+    fn array<const N: usize>(&mut self) -> [u8; N] {
+        self.slice(N).try_into().unwrap()
+    }
+
+    fn vec(&mut self, len: usize) -> Vec<u8> {
+        self.slice(len).to_vec()
+    }
+
+    fn skip(&mut self, len: usize) {
+        self.offset += len;
+    }
+
+    fn u16(&mut self) -> u16 {
+        let value = u16::from_be_bytes(self.data[self.offset..self.offset + 2].try_into().unwrap());
+        self.offset += 2;
+        value
+    }
+
+    fn u32(&mut self) -> u32 {
+        let value = u32::from_be_bytes(self.data[self.offset..self.offset + 4].try_into().unwrap());
+        self.offset += 4;
+        value
+    }
+}
+
+pub struct Index {
+    pub version: u32,
+    pub entries: Vec<IndexEntry>,
+    pub extensions: Vec<IndexExtension>,
+}
+
+impl Index {
+    fn parse(path: PathBuf) -> Result<Self, IndexParseError> {
+        // https://git-scm.com/docs/index-format
+        let data = std::fs::read(path)?;
+        if data.len() < 12 {
+            return Err(IndexParseError::TooShort(data.len()));
+        }
+        let mut reader = Reader::new(&data);
+        if reader.array() != *b"DIRC" {
+            return Err(IndexParseError::InvalidSignature);
+        }
+        let version = reader.u32();
+        if !(2..=4).contains(&version) {
+            return Err(IndexParseError::UnknownVersion(version));
+        }
+        let num_entries = reader.u32();
+        let mut entries = Vec::with_capacity(num_entries as usize);
+        for _ in 0..num_entries {
+            let start_offset = reader.offset;
+            let ctime = Timestamp {
+                seconds: reader.u32(),
+                nanoseconds: reader.u32(),
+            };
+            let mtime = Timestamp {
+                seconds: reader.u32(),
+                nanoseconds: reader.u32(),
+            };
+            let dev = reader.u32();
+            let ino = reader.u32();
+            let mode = reader.u32();
+            let uid = reader.u32();
+            let gid = reader.u32();
+            let size = reader.u32();
+            let sha1 = Sha1(reader.array());
+            let flags = reader.u16();
+            let extended_flags = if version > 2 { reader.u16() } else { 0 };
+            let name_len = (flags & 0xfff) as usize;
+            let name = reader.vec(name_len);
+            assert_eq!(reader.data[reader.offset], 0u8);
+            let name_padding = if version < 4 {
+                8 - ((reader.offset - start_offset) % 8)
+            } else {
+                0
+            };
+            reader.skip(name_padding);
+            entries.push(IndexEntry {
+                ctime,
+                mtime,
+                dev,
+                ino,
+                mode,
+                uid,
+                gid,
+                size,
+                sha1,
+                flags,
+                extended_flags,
+                name,
+            });
+        }
+        let mut extensions = Vec::new();
+        while reader.offset + 8 + 20 < data.len() {
+            let signature = reader.array();
+            let len = reader.u32() as usize;
+            let data = reader.vec(len);
+            extensions.push(IndexExtension { signature, data });
+        }
+        let _checksum = Sha1(reader.array());
+        Ok(Self {
+            version,
+            entries,
+            extensions,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Sha1([u8; 20]);
+
+impl std::fmt::Display for Sha1 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for byte in &self.0 {
+            write!(f, "{:02x}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+pub struct IndexEntry {
+    pub ctime: Timestamp,
+    pub mtime: Timestamp,
+    pub dev: u32,
+    pub ino: u32,
+    pub mode: u32,
+    pub uid: u32,
+    pub gid: u32,
+    pub size: u32,
+    pub sha1: Sha1,
+    pub flags: u16,
+    pub extended_flags: u16,
+    pub name: Vec<u8>,
+}
+
+pub struct IndexExtension {
+    pub signature: [u8; 4],
+    pub data: Vec<u8>,
+}
+
+pub struct Timestamp {
+    pub seconds: u32,
+    pub nanoseconds: u32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 //! Walks a directory, removing every file ignored by git.
 //! Should behave like `git clean -fdx` but hopefully much faster.
 
+use std::collections::HashSet;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
 
 use anyhow::{Context as _, Result};
 use clap::Parser;
 use ignore::gitignore::GitignoreBuilder;
+
+mod git;
 
 #[derive(clap::Parser)]
 struct Args {
@@ -18,37 +21,86 @@ struct Args {
 
     #[clap(short, long)]
     verbose: bool,
+
+    #[clap(short, long)]
+    ignore_index: bool,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
     let progress = indicatif::MultiProgress::new();
-    let ctx = Context { args, progress };
+    let workspace = git::Workspace::find(&args.dir).context("finding git workspace")?;
+
+    let ctx = Context {
+        args,
+        workspace,
+        progress,
+    };
 
     // Annoyingly, ignore has a directory walker, but it only lists *not* ignored files,
     // so we need to basically reimplement it here but with reversed logic.
     // Punt to rayon for parallelism, instead of using crossbeam-deque like it does.
     rayon::in_place_scope(|s| {
-        let mut initial_work = Work::new(&ctx.args.dir);
+        let mut initial_work = Work::new(&ctx);
         initial_work.run(s, &ctx);
     });
+
+    Ok(())
 }
 
 struct Context {
     args: Args,
+    workspace: git::Workspace,
     progress: indicatif::MultiProgress,
+}
+
+fn paths_between<'a>(from: &Path, to: &'a Path) -> impl Iterator<Item = PathBuf> + 'a {
+    assert!(from.is_absolute());
+    assert!(to.is_absolute());
+
+    let mut path = from.to_path_buf();
+    std::iter::from_fn(move || {
+        if path == to {
+            return None;
+        }
+        let component = to.strip_prefix(&path).ok()?.components().next()?;
+        let result = path.clone();
+        path.push(component);
+        Some(result)
+    })
 }
 
 struct Work {
     dir: PathBuf,
     ignore: GitignoreBuilder,
+    index_paths: std::sync::Arc<HashSet<PathBuf>>,
 }
 
 impl Work {
-    fn new(dir: impl AsRef<Path>) -> Self {
+    fn new(context: &Context) -> Self {
+        let mut ignore = GitignoreBuilder::new(&context.workspace.root_dir);
+        ignore.add(context.workspace.git_dir.join("info/exclude"));
+        for dir in paths_between(&context.workspace.root_dir, &context.args.dir) {
+            ignore.add(dir.join(".gitignore"));
+        }
+        let mut index_paths = HashSet::new();
+
+        if !context.args.ignore_index {
+            for entry in context.workspace.index().expect("parsing index").entries {
+                // assume paths are UTF-8
+                if let Ok(path) = std::str::from_utf8(&entry.name) {
+                    let path = context.workspace.root_dir.join(path);
+                    index_paths.insert(path);
+                }
+            }
+        }
+
+        let index_paths = std::sync::Arc::new(index_paths);
+
         Self {
-            dir: dir.as_ref().to_path_buf(),
-            ignore: GitignoreBuilder::new(dir),
+            dir: context.args.dir.clone(),
+            ignore,
+            index_paths,
         }
     }
 
@@ -65,6 +117,10 @@ impl Work {
         for result in std::fs::read_dir(&self.dir).context("reading dir")? {
             let entry = result.context("reading entry")?;
             let path = entry.path();
+            if self.index_paths.contains(&path) {
+                continue;
+            }
+
             let is_dir = entry.file_type().context("getting entry type")?.is_dir();
             if ignore.matched(&path, is_dir).is_ignore() {
                 if ctx.args.dry_run {
@@ -97,6 +153,7 @@ impl Work {
                 let mut work = Work {
                     dir: path,
                     ignore: self.ignore.clone(),
+                    index_paths: self.index_paths.clone(),
                 };
                 s.spawn(move |s| {
                     work.run(s, ctx);
